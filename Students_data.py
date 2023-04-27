ea1f9d9e5170994e4b07926f6060de8f729e2017
Elyza <elyzajanerelucio@gmail.com>
2023-04-27 16:56:48 +0800

    Program outline

---
+++ Students_data.py
@@ -0,0 +1,13 @@
+#Elyza Jane G. Relucio
+#The program reads a file with 20 students' names and GWAs, then outputs the name of the student with the highest GWA.
+
+# open the file containing the student data
+# read the contents of the file into a str variable
+# split the str variable into a list of lines
+# initialize variables to store the highest GWA and the name of the student who got it
+# loop through each line in the file
+# split the line into two parts: the student's name and their GWA
+# convert the GWA from a string to a float
+# if this student has a higher GWA than the current highest GWA, update the variables
+# close the file
+# print the output
