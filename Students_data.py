59500ad4a4e121fde3970680c4525dad01bd5df6
Elyza <elyzajanerelucio@gmail.com>
2023-04-27 18:13:08 +0800

    Create label to display and define function to the second page of design

--- Students_data.py
+++ Students_data.py
@@ -8,15 +8,21 @@ root = tk.Tk()
 root.geometry("800x400")
 root.title("GWA Checker")
 
-# open the file containing the student data
-file = open("students_gwa.txt", "r")
-# read the contents of the file into a str variable
-file_contents = file.read()
-# split the str variable into a list of lines
-lines = file_contents.split("\n")
-# initialize variables to store the highest GWA and the name of the student who got it
-highest_gwa = 0
-highest_gwa_student = ""
+# def function to display the result
+def show_result():
+    # open the file containing the student data
+    file = open("students_gwa.txt", "r")
+
+    # read the contents of the file into a str variable
+    file_contents = file.read()
+
+    # split the str variable into a list of lines
+    lines = file_contents.split("\n")
+
+    # initialize variables to store the highest GWA and the name of the student who got it
+    highest_gwa = 0
+    highest_gwa_student = ""
+
 # loop through each line in the file
 for line in lines:
 # split the line into two parts: the student's name and their GWA
@@ -29,5 +35,13 @@ for line in lines:
             highest_gwa_student = name
 # close the file
 file.close()
-# print the output
-print(f"The student with the highest GWA is {highest_gwa_student} with a GWA of {highest_gwa}.")
+
+# create a label to display the result
+result_label = tk.Label(root, text=f"The student with the highest GWA is {highest_gwa_student} with a GWA of {highest_gwa:.2f}.",bg="violet", font=("Helvetica", 16))
+result_label.pack()
+
+# define function to show the second page
+def show_second_page():
+# remove the first page widgets
+    introduction_label.pack_forget()
+next_button.pack_forget()
