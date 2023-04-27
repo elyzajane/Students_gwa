8def7af523bf1e627556d0e3d3a505cd4b7a1d99
Elyza <elyzajanerelucio@gmail.com>
2023-04-27 17:02:27 +0800

    Open the file containing student's data and read it into str variables

--- Students_data.py
+++ Students_data.py
@@ -2,7 +2,9 @@
 #The program reads a file with 20 students' names and GWAs, then outputs the name of the student with the highest GWA.
 
 # open the file containing the student data
+file = open("students_gwa.txt", "r")
 # read the contents of the file into a str variable
+file_contents = file.read()
 # split the str variable into a list of lines
 # initialize variables to store the highest GWA and the name of the student who got it
 # loop through each line in the file
